61477dd3989a99a27057a5669ba094c5f71b29c5
ephemeral9794 <kthm9794.hiro@gmail.com>
2019-08-16 15:58:52 +0900

    jsonパース

--- src/main/kotlin/com/github/ephemeral9794/niconico/Main.kt
+++ src/main/kotlin/com/github/ephemeral9794/niconico/Main.kt
@@ -1,14 +1,14 @@
 package com.github.ephemeral9794.niconico
 
 import com.github.ephemeral9794.niconico.accessor.NiconicoCrawler
-import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Paths
+import org.json.JSONObject
 
 fun main(args: Array<String>) {
     val res = NiconicoCrawler.get("1565316367")
-    //println(res)
     val json = Util.escapeUnicode(res)
     println(json)
-    //Files.write(Paths.get("./result.json"), json.toByteArray(Charset.defaultCharset()))
+
+    val obj = JSONObject(json)
+    val dmcInfo = obj.getJSONObject("video").getJSONObject("dmcInfo")
+    println(dmcInfo)
 }
